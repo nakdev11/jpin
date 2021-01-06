65a93e7c3ff1b875d8d8cedd493d37f4c4c2b0b6
nakdev11 <nak200ok@gmail.com>
2021-01-07 00:01:36 +0900

    add 20210106

--- src/jpin0106/Hoge.java
+++ src/jpin0106/Hoge.java
@@ -2,6 +2,8 @@ package jpin0106;
 
 public class Hoge {
 
+	static int num;
+
 	public static void hello() {
 		System.out.println("Hello");
 	}
--- src/jpin0106/Sample3.java
+++ src/jpin0106/Sample3.java
@@ -3,6 +3,14 @@ package jpin0106;
 public class Sample3 {
 
 	public static void main(String[] args) {
+
 		Hoge.hello();
+
+		Hoge.num = 10;
+		System.out.println(Hoge.num);
+
 	}
+
+
+
 }
